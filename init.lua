7b2e905f8d30ea5e0df270c6fffdb1ac6e2c0621
Tellegar <matej.stencel@gmail.com>
2025-08-14 11:41:45 +0200

    feat: modular init.lua
    error in 1 file won't break whole configuration, just the single file

--- init.lua
+++ init.lua
@@ -1,9 +1,24 @@
--- Set leader before anything else
+-- set leader before anything else
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Load core config
-require("config.commands")
-require("config.mappings")
-require("config.options")
-require("config.lazy") -- Lazy plugin manager
+-- dedicated python virtual environment
+vim.g.python3_host_prog = vim.fn.expand("~/.config/nvim/.venv/bin/python")
+-- vim.g.python3_host_prog = vim.fn.expand("/home/tellegar/.config/nvim/.venv/bin/python")
+
+-- modular require
+-- * doesnt exit on fail
+-- * instead just notifies of error present in the file
+---@param modname string
+local function Require(modname)
+	local ok, mod = pcall(require, modname)
+	if not ok then
+		vim.notify("Error loading " .. modname .. ": " .. mod, vim.log.levels.ERROR)
+	end
+end
+
+-- load core config
+Require("config.commands")
+Require("config.mappings")
+Require("config.options")
+Require("config.lazy")
