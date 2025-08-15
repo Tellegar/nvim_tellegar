60e07e6601e7747692971b160fdc2df21938040d
Tellegar <matej.stencel@gmail.com>
2025-08-15 12:41:07 +0200

    options: fiddle
    fiddle with the default options
    - more natural search behaviour
    - prettier diff
    - annoying auto comment on insert line

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,9 @@
 local o = vim.opt
--- whitespace visibility
+local g = vim.g
+local autocmd = vim.api.nvim_create_autocmd
+
+----------------------------------------------------------------------------------------------------
+
 o.listchars = 'tab:──╴,space:⋅,trail:~,extends:>,precedes:<'
 o.list = true
 
@@ -11,3 +15,19 @@ o.expandtab = false
 -- line numbers
 o.number = true
 
+-- search behaviour
+o.ignorecase = true
+o.smartcase = true
+
+-- diff
+o.fillchars:append({ diff = "╱" }) -- ╱╲╳ -- ██
+
+-- do not continue comments (e.g. in normal mode pressing o/O won't insert comment)
+local grp = vim.api.nvim_create_augroup("no_auto_comment", { clear = true })
+autocmd({ "FileType" }, {
+	group = grp,
+	pattern = "*",
+	callback = function()
+		vim.opt_local.formatoptions:remove({ "o" })
+	end,
+})
